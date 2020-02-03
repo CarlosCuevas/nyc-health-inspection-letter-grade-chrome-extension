2e5ebf0f93c2801919d66fd166db8d97eac544ca
Carlos Cuevas <ccuevas@etsy.com>
2020-02-03 02:47:09 -0500

    wip 2

--- dist/background.js
+++
@@ -1,5 +0,0 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(details => {
-	chrome.tabs.sendMessage(details.tabId, {action: "url_change"}, response => {
-	  	console.assert(response.message === "ACK");
-	});
-});
